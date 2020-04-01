9cc5aee648f160e5dbff293e846225c9cdb6343d
ThiliniWathsala <o.t.wathsala@gmail.com>
2020-04-01 12:04:32 +0530

    add nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
