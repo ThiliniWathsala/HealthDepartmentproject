a3423f185a866d7e2d2c7d998279105dc6b057a6
ThiliniWathsala <o.t.wathsala@gmail.com>
2020-04-02 13:39:54 +0530

    change nav bar

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-
+  fb: String ="assets/Images/home.png";
   constructor() { }
 
   ngOnInit() {
